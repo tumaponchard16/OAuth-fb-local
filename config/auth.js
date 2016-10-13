45c6362b1b84d7508317ea93224e956741cb96c8
Richard <tumaponchard16@gmail.com>
2016-10-13 14:58:40 +0800

    initial commit

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1748550602062979',
+		'clientSecret': '3989318226ff53119ae7bb9360a204c1',
+		'callbackURL': 'http://locahost:8080/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+}
