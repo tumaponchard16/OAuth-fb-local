ca7bfda9d71903bd2c7280e098707c6648d1168e
Richard <tumaponchard16@gmail.com>
2016-10-13 18:04:06 +0800

    Login with facebook fixed!

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '1748550602062979',
 		'clientSecret': '3989318226ff53119ae7bb9360a204c1',
-		'callbackURL': 'http://locahost:8080/auth/facebook/callback'
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
 	},
 
 	'googleAuth' : {
